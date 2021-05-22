54a7b7f01637fa5ad0ef9445382f6ead60814bac
Siratee K <sirateek@gmail.com>
2021-05-22 21:54:46 +0700

    Implement crash handling &  Improve api configuration in `index.ts` (#8)
    * :bug: Handle the 404 error, the json body parser error to response in the proper form
    
    * Remove unused code
    
    * :sparkles: Rewrite the api config in index.ts to related to the apisList.ts config file.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import { expressMiddleware } from "better-logging";
-import express, { Application } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import config from "./config";
 import preStartScriptHandler from "./preStartScript/preStartScriptHandler";
 import TaApiMiddleWare from "./API/middleWare";
 import getWorkDraftApi from "./API/v1/WorkManagementApi/getWorkDraftApi";
 import submitScoreApi from "./API/v1/WorkManagementApi/submitScoreApi";
+import {
+  APIMethod,
+  ResponseMessage,
+  ResponseStatusCode,
+} from "./API/v1/constructure";
+import apisList from "./API/apisList";
 
 const app: Application = express();
 
@@ -14,20 +20,52 @@ preStartScriptHandler.run();
 // Middleware Setup
 // Use the bodyParser middleware to parse the JSON request
 app.use(express.json());
+
+// Check if the bodyParser has failed parsing Request
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  if (error.type === "entity.parse.failed") {
+    return res.status(ResponseStatusCode.badRequest).send({
+      statusCode: ResponseStatusCode.badRequest,
+      message: ResponseMessage.invalidRequest,
+    });
+  }
+  next();
+});
+
 // Use the better-logging expressMiddleWare to log the incomming request
 app.use(expressMiddleware(console));
 // Check the request permission with TaApiMiddleWare
 app.use(TaApiMiddleWare);
 
-app.get("/", (req, res) => {
-  res.send("Success!");
+// Extract all apis list from the apisList Config file.
+console.info("Extracting all apis from the apisList.ts config file");
+Object.keys(apisList).forEach((apiVersion) => {
+  console.debug(`=> ${apiVersion} apis`);
+  apisList[apiVersion].forEach((apisListElement) => {
+    let apiPath: string = `/${apiVersion}/${apisListElement.path}`;
+    console.debug(` |-> ${apisListElement.name}`);
+    console.debug(` | |- Path : ${apiPath}`);
+    console.debug(` | |- Method : ${apisListElement.apiMethod}`);
+
+    if (apisListElement.apiMethod === APIMethod.GET) {
+      app.get(apiPath, (req, res) => {
+        apisListElement.apiObject.apiHandler(req, res);
+      });
+    }
+    if (apisListElement.apiMethod === APIMethod.POST) {
+      app.post(apiPath, (req, res) => {
+        apisListElement.apiObject.apiHandler(req, res);
+      });
+    }
+  });
 });
 
-app.get("/v1/workManagement/:workId/getWorkDraft/", (req, res) => {
-  getWorkDraftApi.apiHandler(req, res);
-});
-app.post("/v1/workManagement/:workId/submitScore/", (req, res) => {
-  submitScoreApi.apiHandler(req, res);
+// 404-Not Found handler
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(ResponseStatusCode.notFound).send({
+    statusCode: ResponseStatusCode.notFound,
+    message: ResponseMessage.resourceNotFound,
+  });
 });
 
 // Start the server on the configured port
